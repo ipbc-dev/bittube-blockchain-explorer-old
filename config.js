7ebc5207874a9432fee30cecd437ff74a3ca9846
s.skomudek <s.skomudek@devcups.com>
2018-01-09 14:15:09 +0000

    init

---
+++ config.js
@@ -0,0 +1,17 @@
+var api = 'http://pool.ipbc.io:24182';
+var blockTargetInterval = 120;
+var coinUnits = 100000000;
+var symbol = 'IPBC';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+    "ipbc": [
+		["pool.ipbc.io", "http://pool.ipbc.io:8117"]
+    ]
+};
+
+var networkStat2 = {
+    "ipbc": [
+	["pool.ipbc.io", "http://pool.ipbc.io:8117"]
+	    ]
+};
