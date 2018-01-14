78838a0318307452d02f1a876a1a8da994e15036
Miriam Diaz <m.diaz@devcups.com>
2018-01-14 14:24:09 +0000

    design

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://pool.ipbc.io:24182';
+var api = 'https://explorer.ipbc.io:8120';
 var blockTargetInterval = 120;
 var coinUnits = 100000000;
 var symbol = 'IPBC';
@@ -6,12 +6,12 @@ var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
     "ipbc": [
-		["pool.ipbc.io", "http://pool.ipbc.io:8117"]
+		["pool.ipbc.io", "https://pool.ipbc.io:8119"]
     ]
 };
 
 var networkStat2 = {
     "ipbc": [
-	["pool.ipbc.io", "http://pool.ipbc.io:8117"]
+	["pool.ipbc.io", "https://pool.ipbc.io:8119"]
 	    ]
 };
