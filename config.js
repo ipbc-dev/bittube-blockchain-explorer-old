6c4a1d1e51bd4fbb57b91c07469d7f360f3532c1
Saber Maram <s.maram@ipbc.io>
2018-02-23 14:48:50 +0000

    Add pools to config

--- config.js
+++ config.js
@@ -5,13 +5,17 @@ var symbol = 'IPBC';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
-    "ipbc": [
-		["pool.ipbc.io", "https://pool.ipbc.io:8119"]
-    ]
+  "ipbc": [
+    ["pool.ipbc.io", "https://pool.ipbc.io:8119"],
+    ["support.ipbc.io", "https://support.ipbc.io:8119"],
+    ["ipbc.cryptonight.me", "https://ipbc.cryptonight.me:8009"],
+  ]
 };
 
 var networkStat2 = {
-    "ipbc": [
-	["pool.ipbc.io", "https://pool.ipbc.io:8119"]
-	    ]
+  "ipbc": [
+    ["pool.ipbc.io", "https://pool.ipbc.io:8119"],
+    ["support.ipbc.io", "https://support.ipbc.io:8119"],
+    ["ipbc.cryptonight.me", "https://ipbc.cryptonight.me:8009"],
+  ]
 };
